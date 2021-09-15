9c8aa879d4cb2d800ec9fadb73bc96514d60974f
David Velarde <davidmiguel.velarderobles@kpn.com>
2021-09-15 21:48:31 +0200

    Updated prunning

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-git clone https://github.com/davidvpe/evobot
+git clone https://github.com/KiwiPopper/evobot
 
 cd evobot
 
@@ -9,7 +9,7 @@ npm install
 cat config.json.example | \
     jq --arg TOKEN "$TOKEN" \
     --arg YOUTUBETOKEN "$YOUTUBETOKEN" \
-    '.TOKEN = $TOKEN | .YOUTUBE_API_KEY = $YOUTUBETOKEN | .MAX_PLAYLIST_SIZE = 100 | .PREFIX = "!" | .LOCALE = "es" | .DEFAULT_VOLUME = 50' \
+    '.TOKEN = $TOKEN | .YOUTUBE_API_KEY = $YOUTUBETOKEN | .MAX_PLAYLIST_SIZE = 100 | .PREFIX = "!" | .LOCALE = "es" | .DEFAULT_VOLUME = 50 | .PRUNNING = true'  \
     > config.json
 
 cat config.json 
